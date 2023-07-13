e2fc9577fd5af6e02bc0e72517c6db5ddac4e6ee
Joe Bello <joebello123@proton.me>
2023-07-13 13:34:36 -0400

    refactor: path references

--- vite.config.ts
+++ vite.config.ts
@@ -3,31 +3,29 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+const ROOT = path.resolve(__dirname)
+const SRC = `${ROOT}/src`
+const COMPONENTS = `${SRC}/components`
+
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	resolve: {
 		alias: [
-			{ find: '@app', replacement: path.resolve(__dirname, 'src/app') },
-			{ find: '@api', replacement: path.resolve(__dirname, 'src/api') },
-			{ find: '@atoms', replacement: path.resolve(__dirname, 'src/components/atoms') },
-			{
-				find: '@molecules',
-				replacement: path.resolve(__dirname, 'src/components/molecules')
-			},
-			{
-				find: '@organisms',
-				replacement: path.resolve(__dirname, 'src/components/organisms')
-			},
-			{ find: '@pages', replacement: path.resolve(__dirname, 'src/components/pages') },
-			{ find: '@routes', replacement: path.resolve(__dirname, 'src/routes') },
-			{ find: '@utils', replacement: path.resolve(__dirname, 'src/utils') }
+			{ find: '@app', replacement: `${SRC}/app` },
+			{ find: '@api', replacement: `${SRC}/api` },
+			{ find: '@atoms', replacement: `${COMPONENTS}/atoms` },
+			{ find: '@molecules', replacement: `${COMPONENTS}/molecules` },
+			{ find: '@organisms', replacement: `${COMPONENTS}/organisms` },
+			{ find: '@pages', replacement: `${COMPONENTS}/pages` },
+			{ find: '@routes', replacement: `${SRC}/routes` },
+			{ find: '@utils', replacement: `${SRC}/utils` }
 		]
 	},
 	test: {
-		globals: true,
 		environment: 'jsdom',
-		include: ['src/**/*.test.tsx', 'src/**/*.test.ts'],
-		setupFiles: ['./vitest.setup.ts']
+		globals: true,
+		include: [`${SRC}/**/*.test.tsx`, `${SRC}/**/*.test.ts`],
+		setupFiles: [`${ROOT}/vitest.setup.ts`]
 	}
 })
