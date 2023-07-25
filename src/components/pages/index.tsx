5bd10a7a2c6e42ee570cfefe4e1650ada9c8e8bf
Joe Bello <joebello123@proton.me>
2023-07-24 20:23:54 -0400

    wip: `<AccountSettings />`, `<Dashboard />`, `<LogIn />`, `<SignUp />`

--- src/components/pages/index.tsx
+++ src/components/pages/index.tsx
@@ -1,5 +1,9 @@
+import AccountSettings from './AccountSettings'
 import AppError from './AppError'
+import Dashboard from './Dashboard'
 import Home from './Home'
+import LogIn from './LogIn'
 import PageNotFound from './PageNotFound'
+import SignUp from './SignUp'
 
-export { AppError, Home, PageNotFound }
+export { AccountSettings, AppError, Dashboard, Home, LogIn, PageNotFound, SignUp }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,16 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { AppError } from '@pages'
 import Shell from '@shell'
+import accountSettings from './accountSettings'
+import dashboard from './dashboard'
 import home from './home'
+import logIn from './logIn'
 import pageNotFound from './pageNotFound'
+import signUp from './signUp'
 
 const router = createBrowserRouter([
 	{
-		children: [home, pageNotFound],
+		children: [home, accountSettings, dashboard, logIn, signUp, pageNotFound],
 		element: <Shell />,
 		errorElement: <AppError />,
 		path: ''
