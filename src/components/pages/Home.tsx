58ec129137dd4f2f9b404fae3108085b1129fef6
Joe Bello <joebello123@proton.me>
2023-07-27 16:11:30 -0400

    wip: `<Home />` loading state

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,9 +1,18 @@
-import { useLoaderData } from 'react-router-dom'
+import { Suspense } from 'react'
+import { Await, useLoaderData } from 'react-router-dom'
 import { MockData } from '@api'
 import { Board } from '@organisms'
 
 export default function Home() {
-	const mockData = useLoaderData() as MockData
+	const data = useLoaderData() as Awaited<{ mockData: MockData }>
 
-	return <Board data={mockData} />
+	return (
+		<div>
+			<h1>Home</h1>
+			{/* TODO: skeleton */}
+			<Suspense fallback={<h1>Loading...</h1>}>
+				<Await children={(resolved) => <Board data={resolved} />} resolve={data.mockData} />
+			</Suspense>
+		</div>
+	)
 }
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,9 +1,19 @@
-import { MockData, mockData } from '@api'
+import { defer } from 'react-router-dom'
+import { mockData } from '@api'
 import { AppError, Home } from '@pages'
 
-const loader = async function loader(): Promise<MockData> {
-	console.log('Home loading...')
-	return Promise.resolve(mockData)
+const loader = async function loader(): Promise<unknown> {
+	// TODO: loader return type
+	const mockDataPromise = new Promise((resolve) => {
+		return setTimeout(() => {
+			resolve(mockData)
+		}, 3000)
+	})
+
+	// TODO: return value type
+	return defer({
+		mockData: mockDataPromise
+	}) as Awaited<ReturnType<typeof loader>>
 }
 
 export const PATH = '/'
