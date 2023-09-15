3b6a96f2b323336ec17cb77def12ee2144af4f65
Joe Bello <joebello123@proton.me>
2023-09-14 20:01:06 -0400

    config: testing global - `vi`

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,9 +1,13 @@
-import { expect, afterEach } from 'vitest'
+import { afterEach, beforeAll, expect, vi } from 'vitest'
 import { cleanup } from '@testing-library/react'
 import matchers from '@testing-library/jest-dom/matchers'
 
 // extend Vitest's expect with jest-dom matchers
 expect.extend(matchers)
+
+beforeAll(() => {
+	globalThis.vi = vi
+})
 
 // cleanup after each test
 afterEach(() => {
