c90cb947433ffc14c12e2c593c8e35794d595816
Joe Bello <joebello123@proton.me>
2023-10-04 17:54:01 -0400

    refactor: support mantine 7

--- client/src/components/organisms/AuthForm/AuthForm.test.tsx
+++ client/src/components/organisms/AuthForm/AuthForm.test.tsx
@@ -1,50 +1,36 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithProvidersUnauthenticated } from '@utils'
 import AuthForm from './AuthForm'
 
 describe('AuthForm', () => {
 	test('renders', async () => {
-		const onSubmit = () => null
-
-		render(<AuthForm onSubmit={onSubmit} />)
+		renderWithProvidersUnauthenticated(<AuthForm onSubmit={() => null} />)
 		expect(await screen.getByTestId('auth-form')).toBeInTheDocument()
 	})
 
-	test('displays a default message when a message is not provided', async () => {
-		const onSubmit = () => null
-
-		render(<AuthForm onSubmit={onSubmit} />)
+	test('displays a default messaging when messaging is not provided', async () => {
+		renderWithProvidersUnauthenticated(<AuthForm onSubmit={() => null} />)
 		expect(await screen.getByText('Welcome!')).toBeInTheDocument()
-	})
-
-	test('displays the provided message', async () => {
-		const MESSAGE = 'Hello!'
-		const onSubmit = () => null
-
-		render(<AuthForm onSubmit={onSubmit} message={MESSAGE} />)
-		expect(await screen.getByText(MESSAGE)).toBeInTheDocument()
-	})
-
-	test('displays the default "Submit" text when submit button text is not provided', async () => {
-		const onSubmit = () => null
-
-		render(<AuthForm onSubmit={onSubmit} />)
 		expect(await screen.getByText('Submit')).toBeInTheDocument()
 	})
 
-	test('displays the provided submit button text', async () => {
-		const SUBMIT = 'Click!'
-		const onSubmit = () => null
-
-		render(<AuthForm onSubmit={onSubmit} submitText={SUBMIT} />)
-		expect(await screen.getByText(SUBMIT)).toBeInTheDocument()
+	test('displays provided messaging', async () => {
+		const MESSAGE = 'Hello!'
+		const SUBMIT_TEXT = 'Click!'
+		renderWithProvidersUnauthenticated(
+			<AuthForm onSubmit={() => null} message={MESSAGE} submitText={SUBMIT_TEXT} />
+		)
+		expect(await screen.getByText(MESSAGE)).toBeInTheDocument()
+		expect(await screen.getByText(SUBMIT_TEXT)).toBeInTheDocument()
 	})
 
-	test('allows for a secondary action', async () => {
+	test('supports a secondary action', async () => {
 		const TEST_ID = 'testing'
-		const onSubmit = () => null
 		const secondaryAction = <span data-testid={TEST_ID}>Test</span>
 
-		render(<AuthForm onSubmit={onSubmit} SecondaryAction={secondaryAction} />)
+		renderWithProvidersUnauthenticated(
+			<AuthForm onSubmit={() => null} SecondaryAction={secondaryAction} />
+		)
 		expect(await screen.getByTestId(TEST_ID)).toBeInTheDocument()
 	})
 
--- client/src/components/pages/LogIn.tsx
+++ client/src/components/pages/LogIn.tsx
@@ -1,24 +1,59 @@
-import { useContext } from 'react'
-import { Link } from 'react-router-dom'
-import { Text } from '@mantine/core'
+import { useContext, useState } from 'react'
+import { Box, Text, UnstyledButton } from '@mantine/core'
 import { AuthContext } from '@contexts'
-import { SIGN_UP_PATH } from '@routes'
 import { AuthForm } from '@organisms'
 
+type AuthFlow = 'login' | 'signup'
+const LOGIN: AuthFlow = 'login'
+const SIGNUP: AuthFlow = 'signup'
+
 export default function LogIn() {
-	const { logIn } = useContext(AuthContext)
-	const message = <>Welcome Back!{<br />}Please log in to your account.</>
+	const { logIn, signUp } = useContext(AuthContext)
+	const [authFlow, setAuthFlow] = useState<AuthFlow>(LOGIN)
+	const messagingContent = {
+		[LOGIN]: {
+			action: logIn,
+			message: <>Welcome Back!{<br />}Please log in to your account.</>,
+			secondaryMessage: "Don't have an account?",
+			submitText: 'Log in'
+		},
+		[SIGNUP]: {
+			action: signUp,
+			message: <>Welcome!{<br />}Get ready to manage tasks!</>,
+			secondaryMessage: 'Already have an account?',
+			submitText: 'Sign up'
+		}
+	}
+	const content = messagingContent[authFlow]
+
+	const handleChangeAuthFlow = function handleChangeAuthFlow(): void {
+		;(authFlow === LOGIN && setAuthFlow(SIGNUP)) || (authFlow === SIGNUP && setAuthFlow(LOGIN))
+	}
+
+	// TODO: reset form on state change?
 
 	return (
-		<AuthForm
-			SecondaryAction={
-				<Text c="blue" component={Link} to={SIGN_UP_PATH} mx="auto">
-					Don't have an account?
-				</Text>
-			}
-			onSubmit={logIn}
-			message={message}
-			submitText="Log in"
-		/>
+		<Box
+			h="100%"
+			display="flex"
+			mx="auto"
+			style={{
+				alignItems: 'center',
+				justifyContent: 'center'
+			}}
+		>
+			<AuthForm
+				SecondaryAction={
+					<UnstyledButton>
+						<Text c="blue" component="span" onClick={handleChangeAuthFlow} mx="auto">
+							{content.secondaryMessage}
+						</Text>
+					</UnstyledButton>
+				}
+				onSubmit={content.action}
+				message={content.message}
+				submitText={content.submitText}
+			/>
+		</Box>
 	)
 }
